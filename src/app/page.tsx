af30157f2e8452a22a50c3e10529cade4d09e725
unknown <ismael2095951@gmail.com>
2023-10-05 21:17:54 -0300

    cambio posición cartel loading y cartel en /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ function homePage() {
 
   return (
 
-    <div>page</div>
+    <div className="justify-center h-[calc(100vh-12rem)]  flex flex-col items-center">
+      <h1 className="text-8xl font-serif text-sky-800 tracking-widest">BIENVENIDOS !!</h1>
+    </div>
 
   );
 
